74905b248520b84153469d8ccfd9734f2b440b08
Lienz/Llagas <153695889+Neilplay@users.noreply.github.com>
2025-05-25 13:40:27 +0800

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 # Using environment variable for database URL
 database_url = os.getenv(
     "DATABASE_URL",
-    "postgresql://todo_app_render_database_user:s58imWEroNCc141heiJgsblFbwpfD2e1@dpg-cvn5p524d50c73ftpg3g-a.singapore-postgres.render.com/todo_app_render_database"
+    "postgresql://db_qtui_user:kTcS8SVLjEA5bLnTaiHPagxaQKXio9UD@dpg-d0papggdl3ps73ak05tg-a.singapore-postgres.render.com/db_qtui"
 )
 engine = create_engine(database_url, echo=True)
 
